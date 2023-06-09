80e7a0a1df1cf4475a7aefb301034ea6ba082891
hanie ghorbani <hnie.ghorbani@gmail.com>
2023-06-09 18:44:31 +0330

    add massage history

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let massage = document.getElementById("massage")
 let numOfSms = document.getElementById("numOfSms")
 let remainingChr = document.getElementById("remainingChr")
 let btnSend = document.getElementById("btnSend")
-
+let massagesBox = document.querySelector(".massagesBox")
+let inputName = document.querySelector(".inputName")
+let childArray = new Set()
 massage.addEventListener("input", () => {
   let smsCount = parseInt(massage.value.length / 70 + 1)
   numOfSms.innerText = `(${smsCount} پیامک)`
@@ -10,16 +12,83 @@ massage.addEventListener("input", () => {
   remainingChr.innerText = 70 * smsCount - massage.value.length
 })
 
-let blackList = ["عوضی", "بیشعور", "احمق"]
+let blackList = ["کثافت", "عوضی", "بیشعور", "احمق"]
 
 btnSend.addEventListener("click", () => {
-  let words = massage.value.split(" ")
+  if (massage.value.trim() && inputName.value.trim()) {
+    let words = massage.value.split(" ")
+    for (const blackWord of blackList) {
+      if (words.includes(blackWord)) {
+        alert("لطفا شخصیت خود را حفظ و از کلمات مناسب تر استفاده کنید.")
+        return
+      }
+    }
+    alert("پیامک شما ارسال شد .")
 
-  for (const blackWord of blackList) {
-    if (words.includes(blackWord)) {
-      alert("لطفا شخصیت خود را حفظ و از کلمات مناسب تر استفاده کنید.")
+    generateHist(inputName.value, massage.value)
+  } else {
+    alert("لطفا متن پیامک یا نام گیرنده را وارد کنید")
+    massage.value = ""
+  }
+})
+
+massagesBox.addEventListener("click", (e) => {
+  if (e.target.tagName == "H2") {
+    e.target.classList.toggle("active")
+  }
+})
+
+function generateHist(name, sms) {
+  if (!massagesBox.innerHTML) {
+    htmlHistory(name, sms)
+  } else {
+    for (const child of massagesBox.children) {
+      childArray.add(child.children[0].innerText)
+    }
+    if (!childArray.has(inputName.value)) {
+      htmlHistory(name, sms)
+    } else {
+      childArray.forEach((itemChild) => {
+        if (itemChild == name) {
+          for (const child of massagesBox.children) {
+            if (child.firstElementChild.innerText == itemChild) {
+              console.log(child)
+              let newLi = document.createElement("li")
+              newLi.innerText = sms
+              child.lastElementChild.append(newLi)
+            }
+          }
+        }
+      })
     }
   }
 
-  alert('پیامک شما ارسال شد .')
-})
+  inputName.value = ""
+  massage.value = ""
+}
+
+function htmlHistory(name, sms) {
+  let tempOfAccordion = `<div class="accordionItem">
+  <h2 class="recipientName">${name}</h2>
+   <div class="historyOfMassages">
+   <li class="histSMS">${sms}</li>
+</div>
+</div>`
+  massagesBox.insertAdjacentHTML("beforeend", tempOfAccordion)
+}
+
+//
+
+// if (inputName.value == childs.children[0].innerText) {
+//   let newLi = document.createElement("li")
+//   newLi.innerText = massage.value
+//   childs.children[1].append(newLi)
+
+//   massage.value = ""
+//   inputName.value = ""
+//   // console.log(childsArray);
+//   return
+// } else {
+//   generateHist(inputName.value, massage.value)
+//   return
+// }
