33c1dabea31a6d83a31b110414951c647b8fd090
hanie ghorbani <hnie.ghorbani@gmail.com>
2023-06-11 16:31:33 +0330

    add the ability to delete masseges

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ let childArray = new Set()
 let blackList = ["کثافت", "عوضی", "بیشعور", "احمق"]
 
 getLocalStorage()
-
 massage.addEventListener("keyup", (e) => {
   let smsCount = parseInt(massage.value.length / 70 + 1)
   numOfSms.innerText = `(${smsCount} پیامک)`
@@ -47,16 +46,20 @@ function sendSMS() {
     if (!massagesBox.innerHTML) {
       generateHist(inputName.value.trim(), massage.value)
     } else {
+
       for (const child of massagesBox.children) {
-        childArray.add(child.children[0].innerText)
+        childArray.add(child.children[1].innerText)
       }
       if (!childArray.has(inputName.value.trim())) {
+        console.log('ham nam vogod ndrd');
         generateHist(inputName.value.trim(), massage.value)
       } else {
+                console.log(111);
         childArray.forEach((itemChild) => {
           if (itemChild == inputName.value.trim()) {
             for (const child of massagesBox.children) {
-              if (child.firstElementChild.innerText == itemChild) {
+              console.log(child.lastElementChild);
+              if (child.firstElementChild.nextElementSibling.innerText == itemChild) {
                 let newLi = document.createElement("li")
                 newLi.setAttribute("class", "histSMS")
                 newLi.innerText = massage.value
@@ -71,6 +74,7 @@ function sendSMS() {
     massage.value = ""
     massage.focus()
     setLocalStorage(massagesBox)
+    removeAccor()
   } else {
     alert("لطفا متن پیامک یا نام گیرنده را وارد کنید")
   }
@@ -78,6 +82,7 @@ function sendSMS() {
 
 function generateHist(name, sms) {
   let tempOfAccordion = `<div class="accordionItem">
+                          <span class="deleteAccor"></span>
                            <h2 class="recipientName">${name}</h2>
                            <div class="historyOfMassages">
                              <li class="histSMS">${sms}</li>
@@ -88,22 +93,35 @@ function generateHist(name, sms) {
 
 function setLocalStorage(massagesBox) {
   let htmlTempArray = []
-  for (const child of massagesBox.children) {
-    console.log(child.innerHTML)
-    htmlTempArray.push(child.innerHTML)
-    console.log(typeof htmlTempArray)
+  if (!massagesBox.innerHTML) {
     localStorage.setItem("history", JSON.stringify(htmlTempArray))
+  }
+
+  for (const child of massagesBox.children) {
+    htmlTempArray.push(child.innerHTML)
+    localStorage.setItem("history", JSON.stringify(htmlTempArray))
+  }
+}
+
+function removeAccor() {
+  for (const child of massagesBox.children) {
+    child.addEventListener("click", (e) => {
+      if (e.target.tagName == "SPAN") {
+        e.target.parentElement.remove()
+
+        setLocalStorage(massagesBox)
+      }
+    })
   }
 }
 
 function getLocalStorage() {
   let getHistory = JSON.parse(localStorage.getItem("history"))
-
   for (const item of getHistory) {
-    console.log(item)
     let newAccor = document.createElement("div")
     newAccor.setAttribute("class", "accordionItem")
     newAccor.insertAdjacentHTML("beforeend", item)
     massagesBox.append(newAccor)
   }
+  removeAccor()
 }
